cba6696ac7283294ea69a01cdf161cf2641cf68d
Tom <web@tomcanham.co.uk>
2024-09-17 17:08:52 +0100

    Completed Chapter 14 bonus content, adding validation to the edit invoice form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -67,13 +67,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State,  formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields. failed to Edit Invoice',
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const price_in_base_units = amount * 100;
 
   try {
